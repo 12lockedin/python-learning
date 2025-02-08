8f9371f359297a064db9a6d8242a46b849233076
12lockedin <1trillon2@gmail.com>
2025-02-08 19:55:27 +0100

    guest-list

---
+++ 003-introducing-lists/guest-list.py
@@ -0,0 +1,5 @@
+guests = ['Ilya Sutskever', 'Mago More', 'NearCyan', 'Buddha']
+invitation = ', you are invited to dinner at my place this weekend.'
+
+for guest in guests:
+    print(guest + invitation)
