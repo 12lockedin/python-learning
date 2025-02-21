1697c61727b203e391bffcfa648f0b3e37d61db6
12lockedin <1trillon2@gmail.com>
2025-02-21 12:02:27 +0100

    6-1 to 6-3

---
+++ 006-dictionaries/exer-6-1.py
@@ -0,0 +1,13 @@
+"""
+6-1. Person: Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live. You
+should have keys such as first_name, last_name, age, and city. Print each
+piece of information stored in your dictionary.
+"""
+
+lola = {'first_name': 'Lola', 'last_name': 'Fumando', 'age': 25, 'city': 'Un bar'}
+
+print(lola['first_name'])
+print(lola['last_name'])
+print(lola['age'])
+print(lola['city'])
---
+++ 006-dictionaries/exer-6-2.py
@@ -0,0 +1,21 @@
+"""
+6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite
+number for each person, and store each as a value in your dictionary. Print
+each person’s name and their favorite number. For even more fun, poll a few
+friends and get some actual data for your program.
+"""
+
+fav_numbers = {
+    'Mozart': 7,
+    'Bach': 7,
+    'Beethoven': 9,
+    'Vivaldi': 5,
+    'Liszt': 6
+}
+
+print(f"Mozart's fav number is: {fav_numbers['Mozart']}")
+print(f"Bach's fav number is: {fav_numbers['Bach']}")
+print(f"Beethoven's fav number is: {fav_numbers['Beethoven']}")
+print(f"Vivaldi's fav number is: {fav_numbers['Vivaldi']}")
+print(f"Liszt's fav number is: {fav_numbers['Liszt']}")
