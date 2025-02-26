60bf75ccbb69c3e9d4f7ff3dceb20c2e2ca29563
12lockedin <1trillon2@gmail.com>
2025-02-26 17:21:40 +0100

    exercise 15-2

---
+++ part-2-pcc/data-visualization-project/015-generating-data/exer-15-2.py
@@ -0,0 +1,30 @@
+"""
+15-2. Colored Cubes: Apply a colormap to your cubes plot.
+"""
+
+import matplotlib.pyplot as plt
+
+# Create a figure with two subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+five_nums = list(range(1,6))
+five_t_nums = list(range(1,5001))
+
+five_cubes = [num**3 for num in five_nums]
+five_t_cubes = [num**3 for num in five_t_nums]
+
+# First subplot for first 5 numbers
+ax1.scatter(five_nums, five_cubes, c=five_nums, s=50)
+ax1.set_title('Cubes of First 5 Numbers', fontsize=12)
+ax1.set_xlabel('Numbers')
+ax1.set_ylabel('Cubes')
+
+# Second subplot for first 5000 numbers
+ax2.scatter(five_t_nums, five_t_cubes, c=five_t_nums, s=5)
+ax2.set_title('Cubes of First 5000 Numbers', fontsize=12)
+ax2.set_xlabel('Numbers')
+ax2.set_ylabel('Cubes')
+
+# Adjust layout and display
+plt.tight_layout()
+plt.show()
