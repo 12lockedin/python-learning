3c80be99968c7145a6d7e260a11fe0fa2ca003eb
12lockedin <1trillon2@gmail.com>
2025-02-26 13:44:30 +0100

    exercise 11-1

---
+++ 011-testing/city_functions.py
@@ -0,0 +1,3 @@
+def city_country(city, country):
+    """Return City, Country"""
+    return f'{city.title()}, {country.title()}'
---
+++ 011-testing/city_functions_2.py
@@ -0,0 +1,3 @@
+def city_country_2(city, country):
+    """Return City, Country"""
+    return f'{city.title()}, {country.title()}'
